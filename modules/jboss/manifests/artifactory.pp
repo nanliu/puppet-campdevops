e1830b9a2568eeaeb966356b684ea82f20717b0f
rmh@urbancode.com <rmh@urbancode.com>
2011-10-23 15:02:21 -0400

    adding artifactory.war and artifactory.pp to source

---
+++ modules/jboss/manifests/artifactory.pp
@@ -0,0 +1,46 @@
+class jboss(
+  $jboss_file      = $jboss::params::jboss_file,
+  $jboss_file_path = $jboss::params::jboss_file_path
+) inherits jboss::params {
+
+
+  $jboss_path = regsubst($jboss_file, '\.zip', '')
+
+  $home = $::operatingsystem ? {
+    default => '/home',
+  }
+
+  notify { "The value of jboss_path is ${jboss_path}.":}
+  notify { "The value of jboss_file is ${jboss_file}.":}
+
+  user { 'jboss':
+    ensure => present,
+    gid    => 'jboss',
+  }
+
+  group { 'jboss':
+    ensure => present,
+  }
+
+  file { "${home}/jboss":
+    ensure => directory,
+    owner  => 'jboss',
+    group  => 'jboss',
+  }
+
+  file { "${jboss_file_path}/${jboss_file}":
+    ensure => present,
+    source => "puppet:///modules/jboss/${jboss_file}",
+    before => Exec['unzip-jboss'],
+  }
+
+  package {'unzip':
+    ensure => present,
+  }
+
+  exec { 'unzip-jboss':
+    command => "/usr/bin/unzip ${jboss_file_path}/${jboss_file} -d /opt",
+    creates => "/opt/${jboss_path}",
+    require => Package['unzip'],
+  }
+}
