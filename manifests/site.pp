4a1dcf14ed6d78cefe9519c275212e92d7a221a4
Nan Liu <nan@puppetlabs.com>
2011-10-23 15:36:13 -0500

    Fix mysql bug.

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,13 +49,18 @@ node /^db/ {
   class { 'iptables::mysql':
     require => Class['iptables'],
   }
+
+  # Fixing a bug with mysql module.
+  file { '/etc/mysql':
+    ensure => directory,
+  }
+
   $mysql_config = { bind_address      => '127.0.0.1',
                     root_password     => 'campdevops_r0cks',
                     etc_root_password => true }
   class { 'mysql::server':
     config_hash => $mysql_config,
   }
-
   mysql::db { 'dukesbank':
     user     => 'dukesbank',
     password => 'dukesbank',
